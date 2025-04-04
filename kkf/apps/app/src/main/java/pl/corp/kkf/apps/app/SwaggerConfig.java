76c25172090e12f2d1965cb3d9dc09f9f70919e4
technopop1 <75214796+technopop1@users.noreply.github.com>
2025-04-04 21:55:38 +0200

    KKF-14: system application building and modules configuration and base classes initialization

--- kkf/apps/app/src/main/java/pl/corp/kkf/apps/app/SwaggerConfig.java
+++ kkf/apps/app/src/main/java/pl/corp/kkf/apps/app/SwaggerConfig.java
@@ -2,6 +2,10 @@ package pl.corp.kkf.apps.app;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -12,4 +16,10 @@ import org.springframework.context.annotation.Configuration;
         scheme = "bearer"
 )
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("KKF API Docs"));
+    }
 }
